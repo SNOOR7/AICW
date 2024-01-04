a20ad5490fe724ea2dd31e67c65157ad403785a1
Rayhan <91635839+SNOOR7@users.noreply.github.com>
2024-01-04 21:29:51 +0000

    I changed the import to just import keras and solved the error.

--- Coursework.py
+++ Coursework.py
@@ -1,6 +1,7 @@
 import numpy as np
-import tensorflow as tf
-from tf.keras.datasets import mnist
+
+# import tensorflow as tf
+import keras
 
 # This is to load the MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
