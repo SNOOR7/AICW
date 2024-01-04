4dc9733774dca93f5c7b1cd47f87ffc945c84221
Rayhan <91635839+SNOOR7@users.noreply.github.com>
2024-01-04 21:22:38 +0000

    There is an error in the import, so i tried changing the name(failed)

--- Coursework.py
+++ Coursework.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
+from tf.keras.datasets import mnist
 
 # This is to load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
