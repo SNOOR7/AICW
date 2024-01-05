512b7854039dbb600eb7b765c04f92dbe986245f
Rayhan <91635839+SNOOR7@users.noreply.github.com>
2024-01-05 23:49:39 +0000

    Normalise pixels.

--- Coursework.py
+++ Coursework.py
@@ -4,7 +4,12 @@ import keras
 # This is to load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
+# Normalise the pixel values between 0 and 1
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
 
+
+# Activation Functions and Their Derivatives
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
