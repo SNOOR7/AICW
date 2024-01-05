c54f75c2aa5b8fa26da5306a07421ad8759ba18f
Rayhan <91635839+SNOOR7@users.noreply.github.com>
2024-01-05 01:14:08 +0000

    Created a function for sigmoid and the derivative of sigmoid.

--- Coursework.py
+++ Coursework.py
@@ -3,3 +3,11 @@ import keras
 
 # This is to load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
